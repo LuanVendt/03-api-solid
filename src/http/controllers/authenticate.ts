9afeb438a41fd4272f6956395b1d5089a78f9e0c
Luan Vendt <luan.vendt@bitize.com.br>
2023-10-03 13:43:42 -0300

    Implementando JWT no Fastify

--- src/http/controllers/authenticate.ts
+++ src/http/controllers/authenticate.ts
@@ -1,7 +1,7 @@
-import { FastifyRequest, FastifyReply } from 'fastify'
-import { z } from 'zod'
 import { InvalidCredentialsError } from '@/use-cases/errors/invalid-credentials-error'
 import { MakeAuthenticateUseCase } from '@/use-cases/factories/make-authenticate-use-case'
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
 
 export async function authenticate(
   request: FastifyRequest,
@@ -17,9 +17,21 @@ export async function authenticate(
   try {
     const authenticateUseCase = MakeAuthenticateUseCase()
 
-    await authenticateUseCase.execute({
+    const { user } = await authenticateUseCase.execute({
       email,
       password,
+    })
+
+    const token = await reply.jwtSign(
+      {},
+      {
+        sign: {
+          sub: user.id,
+        },
+      },
+    )
+    return reply.status(200).send({
+      token,
     })
   } catch (err) {
     if (err instanceof InvalidCredentialsError) {
@@ -28,6 +40,4 @@ export async function authenticate(
 
     throw err
   }
-
-  return reply.status(200).send()
 }
--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,8 +1,13 @@
 import { FastifyInstance } from 'fastify'
-import { register } from './controllers/register-controller'
 import { authenticate } from './controllers/authenticate'
+import { profile } from './controllers/profile'
+import { register } from './controllers/register-controller'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', register)
+
   app.post('/sessions', authenticate)
+
+  // Authenticated routes
+  app.get('/me', profile)
 }
