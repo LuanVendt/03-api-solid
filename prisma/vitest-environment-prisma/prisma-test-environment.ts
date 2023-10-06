78c904bc006551abb92e1e1cabdceaee83a585ef
Luan Vendt <luan.vendt@bitize.com.br>
2023-10-06 15:36:11 -0300

    Teste E2E do registro

--- prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,16 +1,41 @@
+import { PrismaClient } from '@prisma/client'
+import 'dotenv/config'
+import { execSync } from 'node:child_process'
+import { randomUUID } from "node:crypto"
 import { Environment } from "vitest"
+
+const prisma = new PrismaClient()
+
+function generateDatabaseURL(schema: string) {
+  if (!process.env.DATABASE_URL) {
+    throw new Error('Please provide a DATABASE_URL environment variable')
+  }
+
+  const url = new URL(process.env.DATABASE_URL)
+
+  url.searchParams.set('schema', schema)
+
+  return url.toString()
+}
 
 export default <Environment>(<unknown>{
   name: 'prisma',
 
   async setup() {
-    console.log('Setup')
+    const sechema = randomUUID()
+    const databaseURL = generateDatabaseURL(sechema)
+
+    process.env.DATABASE_URL = databaseURL
+
+    execSync('npx prisma migrate deploy')
+
     return {
       async teardown() {
-        console.log('Teardown')
+        await prisma.$executeRawUnsafe(`DROP SCHEMA IF EXISTS "${sechema}" CASCADE`)
+
+        await prisma.$disconnect()
       },
     }
   },
 })
-
 
